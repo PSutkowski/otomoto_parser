a386a0d0c6b876498f0f3423e4f7c4d05ae44692
Pawel Sutkowski <pawel.sutkowski@se.com>
2021-11-24 16:22:56 +0100

    some progress on url building function, not completed yet

--- func.py
+++ func.py
@@ -9,7 +9,21 @@ def read_yml(yml_name):
             raise exc(f'{yml_name} reading error')
 
 def build_URL_from_filters(yml):
-    url = "wwww.google.pl"
+    prefix = 'https://www.otomoto.pl/osobowe?'
+    asd = yml['filters']['min_price'])
+    sufix = '&search%5Badvanced_search_expanded%5D=1'
+    min_price = f'search%5Bfilter_float_price%3Afrom%5D={yml['filters']['min_price']}'
+    max_price = f'search%5Bfilter_float_price%3Afrom%5D={yml['filters']['max_price']}'
+    seat_list = list(range(yml['filters']['min_pax'], yml['filters']['max_pax'] + 1))
+    seats = [f'&search%5Bfilter_float_nr_seats%5D%5B{index}%5D='{str(seatcount)} for index,seatcount in enumerate(seat_list)]
+    url =  prefix + min_price + max_price + seats +suffix
+        # https://www.otomoto.pl/osobowe?
+        # search%5Bfilter_float_price%3Afrom%5D=25000
+        # &search%5Bfilter_float_price%3Ato%5D=35000
+        # &search%5Bfilter_float_nr_seats%5D%5B0%5D=7
+        # &search%5Bfilter_float_nr_seats%5D%5B1%5D=8
+        # &search%5Bfilter_float_nr_seats%5D%5B2%5D=9
+        # &search%5Badvanced_search_expanded%5D=1
     return url
 
 if __name__ == "__main__":
